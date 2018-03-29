e5c607e764e0d6f9ee3bfc5804662d97b24592e8
Nolan <nemerson17@gmail.com>
2018-03-29 12:14:06 -0700

    Completed displaying list of todo items

---
+++ src/data/todo_items.js
@@ -0,0 +1,22 @@
+export default [
+    {
+        title: 'Can o Beans',
+        details: 'Its a bunch of beans in a tin can'
+    },
+    {
+        title: 'Cobbed Corn',
+        details: 'Its a cob with corn still on it'
+    },
+    {
+        title: 'Haricot Vert',
+        details: 'Green beans hon hon hon'
+    },
+    {
+        title: 'Deep Fried Twinkie',
+        details: 'Ow oof ouch my intestines'
+    },
+    {
+        title: 'Kale Chips',
+        details: 'Who put this junk food here'
+    },
+];
